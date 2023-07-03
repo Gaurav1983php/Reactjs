462b441d60b0ca2a8653d2d5884de9dabe085aef
Gaurav <116250004+Gaurav1983php@users.noreply.github.com>
2023-07-03 11:36:16 +0530

    detail page
    removed link

--- src/component/ListingDetailPage.jsx
+++ src/component/ListingDetailPage.jsx
@@ -39,7 +39,7 @@ function ListingDetailPage (){
                         <div className="card shadow ">
                             <div className="card-body shadow">
                                 {/* <h5 className='card-title text-center h2'>Id : {item1.id}</h5> */}
-                             <Link to={newurl}><img src={item1.image} className="img-fluid" alt="kuh" /></Link>
+                                <img src={item1.image} className="img-fluid" alt="kuh" />
                                 <h6 className='mt-1'>{item1.address}</h6>
                                 <h3 className="text-center">{item1.name}</h3>
                                 <h5>{item1.category}</h5>
